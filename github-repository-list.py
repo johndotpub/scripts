94caef4676847306d315a91184ceef116592a66a
John M <john.miller@brigade.com>
2019-03-25 15:08:55 -0700

    Add github repository list python script
    I also updated some permissions for the other scripts. :)

---
+++ github-repository-list.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+""" Print all repositories for a GitHub organization.
+
+The github3.py module is required::
+
+    $ pip install github3.py
+
+Usage example::
+
+    $ python github-repository-list.py
+
+Advanced use.  This will actually clone all the repos for a
+GitHub organization or user::
+
+    $ for url in $(python list-all-repos.py); do git clone $url; done
+
+"""
+
+import github3
+import getpass
+import pprint
+
+try:
+    # Python 2
+    prompt = raw_input
+except NameError:
+    # Python 3
+    prompt = input
+
+def two_factor():
+    code = ''
+    while not code:
+        code = prompt('Enter 2FA code: ')
+    return code
+
+usr = prompt('Username: ')
+biz = prompt('Organization: ')
+pwd = getpass.getpass(prompt='Password: ', stream=None)
+
+gh = github3.login(usr, pwd, two_factor_callback=two_factor)
+org = gh.organization(biz)
+repos = list(org.repositories(type="all"))
+
+pprint.pprint(repos)
